37b0d23cc9aaf3720a779c2044010f50879cffe3
ProgramatoruLevi <frendlyboss@gmail.com>
2024-12-31 18:53:31 +0200

    Lucrat la [descriere ce ai făcut]

---
+++ src/components/Row.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../axios';
+import './Row.css';
+
+const Row = ({ title, fetchUrl }) => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        className="row__poster"
+                        src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                        alt={movie.title || movie.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Row;
